31293303a12a3a3e98ddd7e911634535d41e20f6
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2023-06-02 02:39:48 -0300

    quarta e estamos avançando

--- pythocrh/enquete/views.py
+++ pythocrh/enquete/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Ola mundo") #o que aparecerá no site deve ser refeeciado na urls
+    return HttpResponse("<h1><u>Ola mundo</u></h1>") #o que aparecerá no site deve ser refeeciado na urls
