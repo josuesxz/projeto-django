dba1cfa82371dc7ba870651e115aba758cb9a1e1
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2023-06-02 02:09:21 -0300

    terceira modificação

--- pythocrh/pythocrh/urls.py
+++ pythocrh/pythocrh/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('enquete/', include('enquete.urls')), 
+    path('enquete/', include('enquete.urls')), #no final disso tudo é toda uma rota até o que aparecerá na tela
     path('admin/', admin.site.urls),
 ]
