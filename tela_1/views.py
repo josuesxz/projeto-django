36b0d145c5fa7b31f0f41750e8959ad295917733
sxjosue <99757018+josuesxz@users.noreply.github.com>
2023-07-03 11:43:25 -0400

    problemas com rotas (verificar isso com mais calma) 03.07_0

--- tela_1/views.py
+++ tela_1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return (render(request, 'index.html'))
+
+def pag2(request):
+    return (render(request, 'pag2.html')) #links das rotas
